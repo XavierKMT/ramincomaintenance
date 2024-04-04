b267841f85221872dab531aa81eabbf93f8f9d5e
XavierKMT <78317478+XavierKMT@users.noreply.github.com>
2024-04-04 05:58:11 -0400

    changed images

--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
-import img1 from './pics/whoweare.jpg';
-import img2 from './pics/ourservices.jpg';
-import img3 from './pics/ourteamforreal.jpg';
+import whoweareimg from './pics/Who we are logo.jpg';
+import ourservicesimg from './pics/Our services logo.jpg';
+import ourteamimg from './pics/Our team logo.jpg';
 import { Grid } from "@mui/material";
 import './About.css';
 import text from './components/TextLanguage.json';
@@ -18,19 +18,19 @@ function About() {
             <div className='about-pics-div'>
                 <Grid container spacing={5} style={{textAlign: 'center'}}>
                     <Grid item sm={4}>
-                        <img src={img1} alt="image1" style={{borderRadius: '50%', maxWidth: '300px', width: '100%'}}/>
+                        <img src={whoweareimg} alt="image1" style={{borderRadius: '50%', maxWidth: '300px', width: '100%'}}/>
                         <h4>{langText.whoweare}</h4>
                         <p>
                             {langText.whowearedesc}
                         </p>
                     </Grid>
                     <Grid item sm={4}>
-                        <img src={img2} alt="image2" style={{borderRadius: '50%', maxWidth: '300px', width: '100%'}}/>
+                        <img src={ourservicesimg} alt="image2" style={{borderRadius: '50%', maxWidth: '300px', width: '100%'}}/>
                         <h4>{langText.ourservices}</h4>
                         <p dangerouslySetInnerHTML={{__html: langText.ourservicesdesc}} />
                     </Grid>
                     <Grid item sm={4}>
-                        <img src={img3} alt="image3" style={{borderRadius: '50%', maxWidth: '300px', width: '100%'}}/>
+                        <img src={ourteamimg} alt="image3" style={{borderRadius: '50%', maxWidth: '300px', width: '100%'}}/>
                         <h4>{langText.ourteam}</h4>
                         <p>
                             {langText.ourteamdesc}
